5a8d0015152bc2414af58f40747755c5ee86a7ae
Alexander Yudin <true.yudin@gmail.com>
2017-04-06 15:36:27 +0300

    Разделил код по функциям, которые выполняют отдельные действия

--- js/stat.js
+++ js/stat.js
@@ -4,36 +4,51 @@ window.renderStatistics = function (ctx, names, times) {
   var histogramHeight = 150;
   var columnWidth = 40;
   var columnIndent = 50;
+  var columnPositionX = 0;
+  var columnPositionY = 0;
   var initialX = 140;
   var initialY = 90;
   var step = histogramHeight / Math.max.apply(null, times);
   var playerColor = 'rgba(255, 0, 0, 1)';
 
-  ctx.fillStyle = 'white';
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(100, 10, 420, 270);
+  function drawText(text, positionX, positionY) {
+    ctx.font = '16px PT Mono';
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+    ctx.shadowColor = 'rgba(0, 0, 0, 0)';
+    ctx.fillStyle = 'black';
+    ctx.fillText(text, positionX, positionY);
+  }
 
-  ctx.fillStyle = 'black';
-  ctx.shadowOffsetX = 0;
-  ctx.shadowOffsetY = 0;
-  ctx.shadowColor = 'rgba(0, 0, 0, 0)';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура, вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
-
-  for (var i = 0; i < names.length; i++) {
-    var columnPositionX = initialX + i * (columnIndent + columnWidth);
-    var columnPositionY = initialY + (histogramHeight - times[i] * step);
+  function drawColumn(coordinateX, coordinateY, columnHeight) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = playerColor;
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
     }
-    ctx.fillRect(columnPositionX, columnPositionY, columnWidth, times[i] * step);
-    ctx.fillStyle = 'black';
-    ctx.fillText(names[i], columnPositionX, initialY + histogramHeight + 20);
-    ctx.fillText(Math.round(times[i]), columnPositionX, columnPositionY - 10);
+    ctx.fillRect(coordinateX, coordinateY, columnWidth, columnHeight);
+  }
+
+  function drawCloud() {
+    ctx.fillStyle = 'white';
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
+    ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillRect(100, 10, 420, 270);
+  }
+
+  function drawHistogram() {
+    columnPositionX = initialX + i * (columnIndent + columnWidth);
+    columnPositionY = initialY + (histogramHeight - times[i] * step);
+    drawColumn(columnPositionX, columnPositionY, times[i] * step);
+    drawText(names[i], columnPositionX, initialY + histogramHeight + 20);
+    drawText(Math.round(times[i]), columnPositionX, columnPositionY - 10);
+  }
+
+  drawCloud();
+  drawText('Ура, вы победили!', 120, 40);
+  drawText('Список результатов:', 120, 60);
+  for (var i = 0; i < names.length; i++) {
+    drawHistogram();
   }
 };
