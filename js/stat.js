6fff83aae65317329657a238f3a99d7023619054
Alexander Yudin <true.yudin@gmail.com>
2017-04-07 16:58:46 +0300

    Убрал лишние переменные, а также их переименовал в функциях

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,11 @@
 'use strict';
 
-function drawCloud(ctx, cloudX, cloudY, cloudWidth, cloudHeight) {
+function drawCloud(ctx, positionX, positionY, cloudWidth, cloudHeight) {
   ctx.fillStyle = 'white';
   ctx.shadowOffsetX = 10;
   ctx.shadowOffsetY = 10;
   ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(cloudX, cloudY, cloudWidth, cloudHeight);
+  ctx.fillRect(positionX, positionY, cloudWidth, cloudHeight);
   ctx.shadowOffsetX = 0;
   ctx.shadowOffsetY = 0;
   ctx.shadowColor = 'rgba(0, 0, 0, 0)';
@@ -17,31 +17,30 @@ function drawText(ctx, text, positionX, positionY) {
   ctx.fillText(text, positionX, positionY);
 }
 
-function drawColumn(ctx, coordinateX, coordinateY, barWidth, barHeight, nickname) {
+function drawColumn(ctx, positionX, positionY, columnWidth, columnHeight, nickname) {
   if (nickname === 'Вы') {
     ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   } else {
     ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
   }
-  ctx.fillRect(coordinateX, coordinateY, barWidth, barHeight);
+  ctx.fillRect(positionX, positionY, columnWidth, columnHeight);
 }
 
-function drawHistogram(ctx, playerNames, playerTimes) {
+function drawHistogram(ctx, names, times) {
   var histogramHeight = 150;
-  var step = histogramHeight / Math.max.apply(null, playerTimes);
+  var step = histogramHeight / Math.max.apply(null, times);
   var columnPositionX = 0;
   var columnPositionY = 0;
   var initialX = 140;
   var initialY = 90;
   var columnWidth = 40;
   var columnIndent = 50;
-  for (var i = 0; i < playerNames.length; i++) {
-    var playerName = playerNames[i];
+  for (var i = 0; i < names.length; i++) {
     columnPositionX = initialX + i * (columnIndent + columnWidth);
-    columnPositionY = initialY + (histogramHeight - playerTimes[i] * step);
-    drawColumn(ctx, columnPositionX, columnPositionY, columnWidth, playerTimes[i] * step, playerName);
-    drawText(ctx, playerNames[i], columnPositionX, initialY + histogramHeight + 20);
-    drawText(ctx, Math.round(playerTimes[i]), columnPositionX, columnPositionY - 10);
+    columnPositionY = initialY + (histogramHeight - times[i] * step);
+    drawColumn(ctx, columnPositionX, columnPositionY, columnWidth, times[i] * step, names[i]);
+    drawText(ctx, names[i], columnPositionX, initialY + histogramHeight + 20);
+    drawText(ctx, Math.round(times[i]), columnPositionX, columnPositionY - 10);
   }
 }
 
