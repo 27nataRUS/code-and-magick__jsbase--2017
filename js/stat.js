12490401de0aacc442ba972f71ee0278ac659460
Alexander Yudin <true.yudin@gmail.com>
2017-04-07 15:39:54 +0300

    Исправил передачу параметров в функциях

--- js/stat.js
+++ js/stat.js
@@ -1,54 +1,54 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  var histogramHeight = 150;
-  var columnWidth = 40;
-  var columnIndent = 50;
-  var columnPositionX = 0;
-  var columnPositionY = 0;
-  var initialX = 140;
-  var initialY = 90;
-  var step = histogramHeight / Math.max.apply(null, times);
-  var playerColor = 'rgba(255, 0, 0, 1)';
 
-  function drawText(text, positionX, positionY) {
-    ctx.font = '16px PT Mono';
-    ctx.shadowOffsetX = 0;
-    ctx.shadowOffsetY = 0;
-    ctx.shadowColor = 'rgba(0, 0, 0, 0)';
-    ctx.fillStyle = 'black';
-    ctx.fillText(text, positionX, positionY);
+  function drawCloud(placeForCloud, cloudX, cloudY, cloudWidth, cloudHeight) {
+    placeForCloud.fillStyle = 'white';
+    placeForCloud.shadowOffsetX = 10;
+    placeForCloud.shadowOffsetY = 10;
+    placeForCloud.shadowColor = 'rgba(0, 0, 0, 0.7)';
+    placeForCloud.fillRect(cloudX, cloudY, cloudWidth, cloudHeight);
+    placeForCloud.shadowOffsetX = 0;
+    placeForCloud.shadowOffsetY = 0;
+    placeForCloud.shadowColor = 'rgba(0, 0, 0, 0)';
   }
 
-  function drawColumn(coordinateX, coordinateY, columnHeight) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = playerColor;
+  function drawText(placeForText, text, positionX, positionY) {
+    placeForText.font = '16px PT Mono';
+    placeForText.fillStyle = 'black';
+    placeForText.fillText(text, positionX, positionY);
+  }
+
+  function drawColumn(placeForColumn, coordinateX, coordinateY, barWidth, barHeight, nickname) {
+    if (nickname === 'Вы') {
+      placeForColumn.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+      placeForColumn.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
     }
-    ctx.fillRect(coordinateX, coordinateY, columnWidth, columnHeight);
+    placeForColumn.fillRect(coordinateX, coordinateY, barWidth, barHeight);
   }
 
-  function drawCloud() {
-    ctx.fillStyle = 'white';
-    ctx.shadowOffsetX = 10;
-    ctx.shadowOffsetY = 10;
-    ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-    ctx.fillRect(100, 10, 420, 270);
+  function drawHistogram(placeForHistogram, playerNames, playerTimes) {
+    var histogramHeight = 150;
+    var step = histogramHeight / Math.max.apply(null, times);
+    var columnPositionX = 0;
+    var columnPositionY = 0;
+    var initialX = 140;
+    var initialY = 90;
+    var columnWidth = 40;
+    var columnIndent = 50;
+    for (var i = 0; i < names.length; i++) {
+      var playerName = playerNames[i];
+      columnPositionX = initialX + i * (columnIndent + columnWidth);
+      columnPositionY = initialY + (histogramHeight - times[i] * step);
+      drawColumn(placeForHistogram, columnPositionX, columnPositionY, columnWidth, playerTimes[i] * step, playerName);
+      drawText(placeForHistogram, playerNames[i], columnPositionX, initialY + histogramHeight + 20);
+      drawText(placeForHistogram, Math.round(playerTimes[i]), columnPositionX, columnPositionY - 10);
+    }
   }
 
-  function drawHistogram() {
-    columnPositionX = initialX + i * (columnIndent + columnWidth);
-    columnPositionY = initialY + (histogramHeight - times[i] * step);
-    drawColumn(columnPositionX, columnPositionY, times[i] * step);
-    drawText(names[i], columnPositionX, initialY + histogramHeight + 20);
-    drawText(Math.round(times[i]), columnPositionX, columnPositionY - 10);
-  }
-
-  drawCloud();
-  drawText('Ура, вы победили!', 120, 40);
-  drawText('Список результатов:', 120, 60);
-  for (var i = 0; i < names.length; i++) {
-    drawHistogram();
-  }
+  drawCloud(ctx, 100, 10, 420, 270);
+  drawText(ctx, 'Ура, вы победили!', 120, 40);
+  drawText(ctx, 'Список результатов:', 120, 60);
+  drawHistogram(ctx, names, times);
 };
